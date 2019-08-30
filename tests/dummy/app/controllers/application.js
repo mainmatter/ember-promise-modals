02d853bcafc83a72156a867161bf2782f9aeb821
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-08-30 14:04:15 +0200

    Implement basic dummy application

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default Controller.extend({
+  modals: service(),
+
+  actions: {
+    showModal() {
+      this.modals.open('modal1');
+    },
+  },
+});
