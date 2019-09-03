b462049a9c2e9d4e0188b2814166b6a5a4dc30ec
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-03 15:30:50 +0200

    Extract `modal` component

---
+++ addon/components/modal.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+import layout from '../templates/components/modal';
+
+export default Component.extend({
+  layout,
+  classNames: ['epm-modal'],
+});
---
+++ app/components/epm-modal.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-promise-modals/components/modal';
