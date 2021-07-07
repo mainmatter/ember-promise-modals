10534bc789273738d3e33d67e8c3b393f25c05c9
nickschot <nickschot@gmail.com>
2021-07-07 15:59:28 +0200

    always close all open modals after a test completes to prevent test waiters from blocking the tests

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -1,21 +1,18 @@
 export function setupPromiseModals(hooks) {
-  let originalValue;
-
   hooks.beforeEach(function () {
     document.documentElement.style.setProperty('--epm-animation-in-duration', `0.0001s`);
     document.documentElement.style.setProperty('--epm-animation-out-duration', `0.0001s`);
 
     this.modals = this.owner.lookup('service:modals');
-
-    originalValue = this.modals.outAnimationTimeout;
-
-    this.modals.set('outAnimationTimeout', 0);
   });
 
   hooks.afterEach(function () {
+    // be sure to close all modals after a test
+    if (this.modals) {
+      this.modals._stack.forEach(modal => modal.close());
+    }
+
     document.documentElement.style.removeProperty('--epm-animation-in-duration', `0.0001s`);
     document.documentElement.style.removeProperty('--epm-animation-out-duration', `0.0001s`);
-
-    this.modals.set('outAnimationTimeout', originalValue);
   });
 }
