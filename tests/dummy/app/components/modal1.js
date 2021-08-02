6827f84b49c0590566783abd6dce05989e5a8d35
aoumiri <anass.oumiri@qonto.eu>
2021-08-02 16:04:09 +0200

    Add a button on the modal1 to be able to open a second one on top of it

--- tests/dummy/app/components/modal1.js
+++ tests/dummy/app/components/modal1.js
@@ -1,5 +1,13 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   tagName: '',
+  modals: service(),
+
+  actions: {
+    showModal2() {
+      this.modals.open('modal2');
+    },
+  },
 });
