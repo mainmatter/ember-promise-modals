dce5401dc42d40967c83cc0a276290bd3a78fea5
Florian Pichler <fp@ylk.gd>
2022-02-04 14:56:41 +0100

    Internal: Extend and apply prettier configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,16 @@
+'use strict';
+
 module.exports = {
   arrowParens: 'avoid',
   printWidth: 120,
   singleQuote: true,
   trailingComma: 'all',
+  overrides: [
+    {
+      files: '**/*.{hbs,yml,yaml}',
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
