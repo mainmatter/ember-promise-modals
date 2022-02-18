6fdbca9b7bacef8db73dc6df4bd5697aafdc3656
nickschot <nickschot@gmail.com>
2022-02-18 13:50:16 +0100

    Use @ember/test-helpers v1 for ember 2.18

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,6 +11,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~2.18.0',
+            '@ember/test-helpers': '^1.7.3',
           },
         },
       },
