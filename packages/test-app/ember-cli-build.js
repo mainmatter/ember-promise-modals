d27e41a61c247ba7f9839995717f87273270ce17
Michael Kerr <michael.kerr@mainmatter.com>
2025-07-01 00:26:42 +0200

    use embroider build

--- packages/test-app/ember-cli-build.js
+++ packages/test-app/ember-cli-build.js
@@ -1,5 +1,4 @@
 'use strict';
-const { maybeEmbroider } = require('@embroider/test-setup');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
@@ -13,5 +12,7 @@ module.exports = function (defaults) {
     behave.
   */
 
-  return maybeEmbroider(app);
+  const { Webpack } = require('@embroider/webpack');
+
+  return require('@embroider/compat').compatBuild(app, Webpack);
 };
