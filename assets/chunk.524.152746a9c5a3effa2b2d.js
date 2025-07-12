ddc18e9de6057acedddaaec2d80e2b47099542eb
pichfl <pichfl@users.noreply.github.com>
2025-07-12 10:53:12 +0000

    Deploying to gh-pages from @ mainmatter/ember-promise-modals@a06de1bd5cd1d59fac8dd2e6885b0de10c86c75e 🚀

--- assets/chunk.524.152746a9c5a3effa2b2d.js
+++ assets/chunk.524.152746a9c5a3effa2b2d.js
@@ -39,15 +39,15 @@ throw new TypeError("@@toPrimitive must return a primitive value.")}return Strin
 return"symbol"==typeof t?t:t+""}(t))in e?Object.defineProperty(e,t,{value:null,enumerable:!0,configurable:!0,writable:!0}):e[t]=null}(this,"modal")}compute([e,t,o],{close:i}){this.showModal(e,t,o,i)}async showModal(e,t,o,i){this.modal=this.modals.open(e,t,o)
 let s=await this.modal
 this.modal=null,i&&i(s)}hideModal(){this.modal?.close()}willDestroy(){this.hideModal(),super.willDestroy(...arguments)}}s=m,(0,a.g)(s.prototype,"modals",[n.inject])},130:e=>{"use strict"
-e.exports=require("@ember/destroyable")},256:(e,t,o)=>{e.exports=function(){var e=_eai_d,t=_eai_r
-function i(e){return e&&e.__esModule?e:Object.assign({default:e},e)}window.emberAutoImportDynamic=function(e){return 1===arguments.length?t("_eai_dyn_"+e):t("_eai_dynt_"+e)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(e){return t("_eai_sync_"+e)(Array.prototype.slice.call(arguments,1))},e("ember-promise-modals/components/epm-modal",["@ember/object","@ember/service","@glimmer/component","@ember/application","@ember/modifier","@ember/destroyable","@ember/component","@ember/template-factory"],function(){return i(o(83))}),e("ember-promise-modals/components/epm-modal-container",["@ember/service","@glimmer/component","@ember/component","@ember/template-factory"],function(){return i(o(63))}),e("ember-promise-modals/helpers/open-modal",["@ember/service"],function(){return i(o(114))}),e("ember-promise-modals/services/modals",["@ember/object","@ember/service"],function(){return i(o(21))})}()},294:e=>{"use strict"
+e.exports=require("@ember/destroyable")},294:e=>{"use strict"
 e.exports=require("@ember/application")},377:e=>{"use strict"
 e.exports=require("@ember/modifier")},465:e=>{"use strict"
 e.exports=require("@ember/template-factory")},471:e=>{"use strict"
-e.exports=require("@ember/object")},517:function(e,t){window._eai_r=require,window._eai_d=define},663:e=>{"use strict"
+e.exports=require("@ember/object")},522:function(e,t){window._eai_r=require,window._eai_d=define},663:e=>{"use strict"
 e.exports=require("@ember/component")},735:e=>{"use strict"
 e.exports=require("@ember/service")},855:e=>{"use strict"
-e.exports=require("@glimmer/component")}},o={}
+e.exports=require("@glimmer/component")},879:(e,t,o)=>{e.exports=function(){var e=_eai_d,t=_eai_r
+function i(e){return e&&e.__esModule?e:Object.assign({default:e},e)}window.emberAutoImportDynamic=function(e){return 1===arguments.length?t("_eai_dyn_"+e):t("_eai_dynt_"+e)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(e){return t("_eai_sync_"+e)(Array.prototype.slice.call(arguments,1))},e("ember-promise-modals/components/epm-modal",["@ember/object","@ember/service","@glimmer/component","@ember/application","@ember/modifier","@ember/destroyable","@ember/component","@ember/template-factory"],function(){return i(o(83))}),e("ember-promise-modals/components/epm-modal-container",["@ember/service","@glimmer/component","@ember/component","@ember/template-factory"],function(){return i(o(63))}),e("ember-promise-modals/helpers/open-modal",["@ember/service"],function(){return i(o(114))}),e("ember-promise-modals/services/modals",["@ember/object","@ember/service"],function(){return i(o(21))})}()}},o={}
 function i(e){var s=o[e]
 if(void 0!==s)return s.exports
 var r=o[e]={exports:{}}
@@ -64,6 +64,6 @@ var t=(t,o)=>{var s,r,[n,a,l]=o,m=0
 if(n.some(t=>0!==e[t])){for(s in a)i.o(a,s)&&(i.m[s]=a[s])
 if(l)var d=l(i)}for(t&&t(o);m<n.length;m++)r=n[m],i.o(e,r)&&e[r]&&e[r][0](),e[r]=0
 return i.O(d)},o=globalThis.webpackChunk_ember_auto_import_=globalThis.webpackChunk_ember_auto_import_||[]
-o.forEach(t.bind(null,0)),o.push=t.bind(null,o.push.bind(o))})(),i.O(void 0,[249],()=>i(517))
-var s=i.O(void 0,[249],()=>i(256))
+o.forEach(t.bind(null,0)),o.push=t.bind(null,o.push.bind(o))})(),i.O(void 0,[249],()=>i(522))
+var s=i.O(void 0,[249],()=>i(879))
 s=i.O(s),__ember_auto_import__=s})()
